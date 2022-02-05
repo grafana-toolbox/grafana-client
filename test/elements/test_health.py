dcba7c75ca469a204cb6dee0ff1edb245b292aa5
peekjef72 <jfpik78@gmail.com>
2022-02-05 19:30:45 +0100

    add health and datasource proxy

---
+++ test/elements/test_health.py
@@ -0,0 +1,33 @@
+import unittest
+
+import requests_mock
+
+from grafana_client import GrafanaApi
+from grafana_client.client import (
+    GrafanaBadInputError,
+    GrafanaClientError,
+    GrafanaServerError,
+    GrafanaUnauthorizedError,
+)
+
+
+class HealthTestCase(unittest.TestCase):
+    def setUp(self):
+        self.grafana = GrafanaApi(("admin", "admin"), host="localhost", url_path_prefix="", protocol="http")
+
+    @requests_mock.Mocker()
+    def test_search_dashboards(self, m):
+        m.get(
+            "http://localhost/api/health",
+            json=[
+                {
+                    "commit": "6f8c1d9fe4",
+                    "database": "ok",
+                    "version": "7.5.11"
+                }
+            ],
+        )
+
+        result = self.grafana.health.check()
+        self.assertEqual(result[0]["database"], 'ok')
+        self.assertEqual(len(result), 1)
