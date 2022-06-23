6f4f44083d9ca5cb23f1844ecc07b638ec739a6b
Andreas Motl <andreas.motl@panodata.org>
2022-06-23 21:57:41 +0200

    Add `GrafanaApi.connect()` and `GrafanaApi.version()`

--- grafana_client/__init__.py
+++ grafana_client/__init__.py
@@ -1,1 +1,4 @@
+import warnings
+
+warnings.filterwarnings("ignore", category=Warning, message="distutils Version classes are deprecated")
 from .api import GrafanaApi
--- test/test_grafana_client.py
+++ test/test_grafana_client.py
@@ -9,7 +9,7 @@ else:
 import requests
 
 from grafana_client.api import GrafanaApi
-from grafana_client.client import TokenAuth
+from grafana_client.client import GrafanaClientError, TokenAuth
 
 
 class MockResponse:
@@ -94,6 +94,36 @@ class TestGrafanaClient(unittest.TestCase):
             protocol="https",
         )
         self.assertTrue(isinstance(grafana.client.auth, TokenAuth))
+
+    def test_tokenauth(self):
+        tokenauth = TokenAuth("VerySecretToken")
+        request = requests.Request()
+        tokenauth(request)
+        self.assertEqual(request.headers["Authorization"], "Bearer VerySecretToken")
+
+    @patch("grafana_client.client.GrafanaClient.__getattr__")
+    def test_grafana_client_connect_success(self, mock_get):
+        payload = {"commit": "14e988bd22", "database": "ok", "version": "9.0.1"}
+        mock_get.return_value = Mock()
+        mock_get.return_value.return_value = payload
+        grafana = GrafanaApi(auth=None, host="localhost", url_path_prefix="", protocol="http", port="3000")
+        grafana_info = grafana.connect()
+        self.assertEqual(grafana_info, payload)
+
+    def test_grafana_client_connect_failure(self):
+        grafana = GrafanaApi(auth=None, host="localhost", url_path_prefix="", protocol="http", port="32425")
+        self.assertRaises(requests.exceptions.ConnectionError, lambda: grafana.connect())
+
+    @patch("grafana_client.client.GrafanaClient.__getattr__")
+    def test_grafana_client_version(self, mock_get):
+        mock_get.return_value = Mock()
+        mock_get.return_value.return_value = {"commit": "14e988bd22", "database": "ok", "version": "9.0.1"}
+        grafana = GrafanaApi(auth=None, host="localhost", url_path_prefix="", protocol="http", port="3000")
+        self.assertEqual(grafana.version, "9.0.1")
+
+    def test_grafana_client_non_json_response(self):
+        grafana = GrafanaApi.from_url("https://httpbin.org/html")
+        self.assertRaises(GrafanaClientError, lambda: grafana.connect())
 
 
 if __name__ == "__main__":
