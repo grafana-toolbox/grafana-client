1d852bc105be2b6c9f333aba395a7f8f1f033d33
Andreas Motl <andreas.motl@panodata.org>
2022-12-02 12:09:40 +0100

    Improve "Organization preferences"
    - Add model-based conversation, using `UserOrganizationPreferences`.
    - Add `organization_preferences_patch`.
    - Deprecate `organization_preference_get` and
      `organization_preference_update` in favor of
      `organization_preferences_get` and
      `organization_preferences_update` (singular vs. plural).

--- grafana_client/model.py
+++ grafana_client/model.py
@@ -67,3 +67,32 @@ class DatasourceHealthResponse:
         data = self.asdict()
         del data["response"]
         return data
+
+
+@dataclasses.dataclass
+class UserOrganizationPreferences:
+    """
+    Request/response model for user- and organization-preferences.
+
+    https://grafana.com/docs/grafana/latest/developers/http_api/preferences/
+    """
+
+    homeDashboardId: Optional[int] = None
+    homeDashboardUID: Optional[str] = None
+    locale: Optional[str] = None
+    theme: Optional[str] = None
+    timezone: Optional[str] = None
+    weekStart: Optional[str] = None
+
+    def asdict(self, filter_none=False):
+        if filter_none:
+            return dataclasses.asdict(self, dict_factory=self.dict_factory_filter_none)
+        else:
+            return dataclasses.asdict(self)
+
+    @staticmethod
+    def dict_factory_filter_none(seq=None, **kwargs):
+        seq = [item for item in seq if item[1] is not None]
+        data = dict(seq)
+        data.update(kwargs)
+        return data
