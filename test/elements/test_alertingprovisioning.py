fda367bb11b2bb716bd609c55093d93f4aedb21d
Andreas Motl <andreas.motl@panodata.org>
2022-12-02 22:15:10 +0100

    Alerting Provisioning: Add support for `X-Disable-Provenance` header
    Grafana Labs says:
    
    > To be able to modify alerts created via API within the UI later,
    > we are enabling a workaround within the Alerting Provisioning API.
    > Just add the `x-disable-provenance` header to the following requests:
    >
    > - POST /api/v1/provisioning/alert-rules
    > - PUT /api/v1/provisioning/alert-rules/{UID}

--- test/elements/test_alertingprovisioning.py
+++ test/elements/test_alertingprovisioning.py
@@ -38,7 +38,7 @@ class AlertingProvisioningTestCase(unittest.TestCase):
         self.assertEqual(response["uid"], "bUUGqLiVk")
 
     @requests_mock.Mocker()
-    def test_create_alertrule(self, m):
+    def test_create_alertrule_default(self, m):
         m.post(
             "http://localhost/api/v1/provisioning/alert-rules",
             json=ALERTRULE,
@@ -47,8 +47,29 @@ class AlertingProvisioningTestCase(unittest.TestCase):
         response = self.grafana.alertingprovisioning.create_alertrule(ALERTRULE)
         self.assertEqual(response["uid"], "bUUGqLiVk")
 
+        # Verify request header.
+        history = m.request_history
+        headers = history[0].headers
+        self.assertNotIn("X-Disable-Provenance", headers)
+
     @requests_mock.Mocker()
-    def test_update_alertrule(self, m):
+    def test_create_alertrule_disable_provenance(self, m):
+        m.post(
+            "http://localhost/api/v1/provisioning/alert-rules",
+            json=ALERTRULE,
+        )
+
+        response = self.grafana.alertingprovisioning.create_alertrule(ALERTRULE, disable_provenance=True)
+        self.assertEqual(response["uid"], "bUUGqLiVk")
+
+        # Verify request header.
+        history = m.request_history
+        headers = history[0].headers
+        self.assertIn("X-Disable-Provenance", headers)
+        self.assertEqual(headers["X-Disable-Provenance"], "true")
+
+    @requests_mock.Mocker()
+    def test_update_alertrule_default(self, m):
         m.put(
             "http://localhost/api/v1/provisioning/alert-rules/bUUGqLiVk",
             json=ALERTRULE,
@@ -56,3 +77,26 @@ class AlertingProvisioningTestCase(unittest.TestCase):
 
         response = self.grafana.alertingprovisioning.update_alertrule(alertrule_uid="bUUGqLiVk", alertrule=ALERTRULE)
         self.assertEqual(response["uid"], "bUUGqLiVk")
+
+        # Verify request header.
+        history = m.request_history
+        headers = history[0].headers
+        self.assertNotIn("X-Disable-Provenance", headers)
+
+    @requests_mock.Mocker()
+    def test_update_alertrule_disable_provenance(self, m):
+        m.put(
+            "http://localhost/api/v1/provisioning/alert-rules/bUUGqLiVk",
+            json=ALERTRULE,
+        )
+
+        response = self.grafana.alertingprovisioning.update_alertrule(
+            alertrule_uid="bUUGqLiVk", alertrule=ALERTRULE, disable_provenance=True
+        )
+        self.assertEqual(response["uid"], "bUUGqLiVk")
+
+        # Verify request header.
+        history = m.request_history
+        headers = history[0].headers
+        self.assertIn("X-Disable-Provenance", headers)
+        self.assertEqual(headers["X-Disable-Provenance"], "true")
