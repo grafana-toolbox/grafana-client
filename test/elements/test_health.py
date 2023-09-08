e2745a0e396b3ff1a56f2f645b8be36cb2e114d9
Andreas Motl <andreas.motl@panodata.org>
2023-09-08 16:43:34 +0200

    Tests: Fix healthcheck test

--- test/elements/test_health.py
+++ test/elements/test_health.py
@@ -10,12 +10,12 @@ class HealthTestCase(unittest.TestCase):
         self.grafana = GrafanaApi(("admin", "admin"), host="localhost", url_path_prefix="", protocol="http")
 
     @requests_mock.Mocker()
-    def test_search_dashboards(self, m):
+    def test_healthcheck(self, m):
         m.get(
             "http://localhost/api/health",
-            json=[{"commit": "6f8c1d9fe4", "database": "ok", "version": "7.5.11"}],
+            json={"commit": "6f8c1d9fe4", "database": "ok", "version": "7.5.11"},
         )
 
         result = self.grafana.health.check()
-        self.assertEqual(result[0]["database"], "ok")
-        self.assertEqual(len(result), 1)
+        self.assertEqual(result["database"], "ok")
+        self.assertEqual(len(result), 3)
