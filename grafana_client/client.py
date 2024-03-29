277f87a6fb5b58d79e6ae88b0a588a8ca2a21ecb
TAHRI Ahmed R <Ousret@users.noreply.github.com>
2024-03-29 05:59:46 +0100

    Update grafana_client/client.py
    Co-authored-by: Andreas Motl <andreas.motl@elmyra.de>

--- grafana_client/client.py
+++ grafana_client/client.py
@@ -247,7 +247,7 @@ class AsyncGrafanaClient(GrafanaClient):
         async def __request_runner(url, json=None, data=None, headers=None, accept_empty_json=False):
             __url = self._make_url(url)
             # Sanity checks.
-            GrafanaClient._ensure_valid_json_arg(json)
+            self._ensure_valid_json_arg(json)
 
             try:
                 r = await self.s.request(
