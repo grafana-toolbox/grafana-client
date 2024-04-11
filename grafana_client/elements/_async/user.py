513eee4a8c33473a006d20b6e64d1f435bfac66d
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2024-04-11 14:47:54 +0530

    Implement teams read endpoints for user and actual user.

--- grafana_client/elements/_async/user.py
+++ grafana_client/elements/_async/user.py
@@ -85,6 +85,15 @@ class Users(Base):
         get_user_organisations_path = "/users/%s/orgs" % user_id
         return await self.client.GET(get_user_organisations_path)
 
+    async def get_user_teams(self, user_id):
+        """
+
+        :param user_id:
+        :return:
+        """
+        get_user_teams_path = "/users/%s/teams" % user_id
+        return await self.client.GET(get_user_teams_path)
+
 
 class User(Base):
     def __init__(self, client):
@@ -144,6 +153,14 @@ class User(Base):
         """
         get_actual_user_organisations_path = "/user/orgs"
         return await self.client.GET(get_actual_user_organisations_path)
+
+    async def get_actual_user_teams(self):
+        """
+
+        :return:
+        """
+        get_actual_user_teams_path = "/user/teams"
+        return await self.client.GET(get_actual_user_teams_path)
 
     async def star_actual_user_dashboard(self, dashboard_id):
         """
