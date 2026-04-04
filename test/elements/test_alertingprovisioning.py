c6cae8e8ec14e25e30fb2f7af9dd5eeccdb906fb
Andreas Motl <andreas.motl@panodata.org>
2026-04-04 18:53:16 +0200

    Tests: Integration tests for alerting provisioning

--- test/elements/test_alertingprovisioning.py
+++ test/elements/test_alertingprovisioning.py
@@ -1,142 +1,128 @@
+"""
+https://www.percona.com/blog/copying-alert-rules-from-one-percona-monitoring-and-management-server-to-another/
+"""
+
+import sys
 import unittest
 
-from grafana_client import GrafanaApi
+import pytest
+from verlib2 import Version
 
-from ..compat import requests_mock
+from grafana_client.client import GrafanaClientError, GrafanaServerError
 
 ALERTRULE = {
-    "id": 2,
     "uid": "bUUGqLiVk",
+    "provenance": "api",
     "orgID": 1,
-    "folderUID": "4",
+    "folderUID": "ap-folder",
     "ruleGroup": "Any",
     "title": "ApiAlerts",
     "condition": "B",
-    "data": [],
+    "data": [
+        {
+            "refId": "A",
+            "queryType": "",
+            "relativeTimeRange": {"from": 600, "to": 0},
+            "datasourceUid": "PA58DA793C7250F1B",
+            "model": {
+                "expr": "max_over_time(mysql_global_status_threads_connected[5m])",
+                "instant": True,
+                "intervalMs": 1000,
+                "maxDataPoints": 43200,
+                "refId": "A",
+            },
+        }
+    ],
     "updated": "2022-08-17T13:00:21Z",
     "noDataState": "NoData",
     "execErrState": "Alerting",
-    "for": 300000000000,
+    "for": "5m",
     "labels": {"alertGroup": "any", "region": "eu-west-1"},
 }
 
+pytestmark = pytest.mark.integration
 
+
+@unittest.skipIf("unittest" in sys.argv[0], "Skipping unittest, please use pytest")
 class AlertingProvisioningTestCase(unittest.TestCase):
-    def setUp(self):
-        self.grafana = GrafanaApi(("admin", "admin"), host="localhost", url_path_prefix="", protocol="http")
+    @pytest.fixture(autouse=True)
+    def use_fixtures(self, grafana_provisioned):
+        self.grafana = grafana_provisioned
+        if Version(self.grafana.version) < Version("9"):
+            pytest.skip("Alerting provisioning supported by Grafana 9 and higher.")
 
-    @requests_mock.Mocker()
-    def test_get_alertrules_all(self, m):
-        m.get(
-            "http://localhost/api/v1/provisioning/alert-rules",
-            json=[ALERTRULE],
-        )
+        # Prune alerting rules and mute timings.
+        for alertrule in self.grafana.alertingprovisioning.get_alertrules_all():
+            self.grafana.alertingprovisioning.delete_alertrule(alertrule["uid"])
+        for mute_timing in self.grafana.alertingprovisioning.get_mute_timings():
+            self.grafana.alertingprovisioning.delete_mute_timing(mute_timing["name"])
+
+        # Provision single alerting rule.
+        self.folder = self.grafana.folder.create_folder("alert-folder", uid="ap-folder")
+        self.grafana.alertingprovisioning.create_alertrule(ALERTRULE)
+        self.alertrule_uid = ALERTRULE["uid"]
+
+    def test_get_alertrules_all(self):
         response = self.grafana.alertingprovisioning.get_alertrules_all()
-        self.assertEqual(response[0]["uid"], "bUUGqLiVk")
+        self.assertEqual(response[0]["uid"], self.alertrule_uid)
 
-    @requests_mock.Mocker()
-    def test_get_alertrule(self, m):
-        m.get(
-            "http://localhost/api/v1/provisioning/alert-rules/bUUGqLiVk",
-            json=ALERTRULE,
-        )
-        response = self.grafana.alertingprovisioning.get_alertrule("bUUGqLiVk")
-        self.assertEqual(response["uid"], "bUUGqLiVk")
+    def test_get_alertrule(self):
+        response = self.grafana.alertingprovisioning.get_alertrule(self.alertrule_uid)
+        self.assertEqual(response["uid"], self.alertrule_uid)
 
-    @requests_mock.Mocker()
-    def test_delete_alertrule(self, m):
-        m.delete(
-            "http://localhost/api/v1/provisioning/alert-rules/bUUGqLiVk",
-            json={"uid": "bUUGqLiVk"},
-        )
-        response = self.grafana.alertingprovisioning.delete_alertrule("bUUGqLiVk")
-        self.assertEqual(response["uid"], "bUUGqLiVk")
+    def test_delete_alertrule(self):
+        self.grafana.alertingprovisioning.delete_alertrule(self.alertrule_uid)
+        with self.assertRaises(GrafanaClientError) as excinfo:
+            self.grafana.alertingprovisioning.get_alertrule(self.alertrule_uid)
+        self.assertEqual(excinfo.exception.status_code, 404)
+        self.assertRegex(excinfo.exception.message, "Client Error 404: (null)?")
 
-    @requests_mock.Mocker()
-    def test_create_alertrule_default(self, m):
-        m.post(
-            "http://localhost/api/v1/provisioning/alert-rules",
-            json=ALERTRULE,
-        )
-
-        response = self.grafana.alertingprovisioning.create_alertrule(ALERTRULE)
-        self.assertEqual(response["uid"], "bUUGqLiVk")
-
-        # Verify request header.
-        history = m.request_history
-        headers = history[0].headers
-        self.assertNotIn("X-Disable-Provenance", headers)
-
-    @requests_mock.Mocker()
-    def test_create_alertrule_disable_provenance(self, m):
-        m.post(
-            "http://localhost/api/v1/provisioning/alert-rules",
-            json=ALERTRULE,
-        )
-
-        response = self.grafana.alertingprovisioning.create_alertrule(ALERTRULE, disable_provenance=True)
-        self.assertEqual(response["uid"], "bUUGqLiVk")
-
-        # Verify request header.
-        history = m.request_history
-        headers = history[0].headers
-        self.assertIn("X-Disable-Provenance", headers)
-        self.assertEqual(headers["X-Disable-Provenance"], "true")
-
-    @requests_mock.Mocker()
-    def test_update_alertrule_default(self, m):
-        m.put(
-            "http://localhost/api/v1/provisioning/alert-rules/bUUGqLiVk",
-            json=ALERTRULE,
-        )
-
-        response = self.grafana.alertingprovisioning.update_alertrule(alertrule_uid="bUUGqLiVk", alertrule=ALERTRULE)
-        self.assertEqual(response["uid"], "bUUGqLiVk")
-
-        # Verify request header.
-        history = m.request_history
-        headers = history[0].headers
-        self.assertNotIn("X-Disable-Provenance", headers)
-
-    @requests_mock.Mocker()
-    def test_update_alertrule_disable_provenance(self, m):
-        m.put(
-            "http://localhost/api/v1/provisioning/alert-rules/bUUGqLiVk",
-            json=ALERTRULE,
-        )
-
+    def test_update_alertrule_standard(self):
         response = self.grafana.alertingprovisioning.update_alertrule(
-            alertrule_uid="bUUGqLiVk", alertrule=ALERTRULE, disable_provenance=True
+            alertrule_uid=self.alertrule_uid, alertrule=ALERTRULE
         )
-        self.assertEqual(response["uid"], "bUUGqLiVk")
+        self.assertEqual(response["uid"], self.alertrule_uid)
 
-        # Verify request header.
-        history = m.request_history
-        headers = history[0].headers
-        self.assertIn("X-Disable-Provenance", headers)
-        self.assertEqual(headers["X-Disable-Provenance"], "true")
+    def test_create_alertrule_disable_provenance(self):
+        alertrule = ALERTRULE.copy()
+        alertrule["uid"] = "ng"
+        alertrule["title"] = "ng"
+        response = self.grafana.alertingprovisioning.create_alertrule(alertrule, disable_provenance=True)
+        self.assertEqual(response["uid"], "ng")
 
-    @requests_mock.Mocker()
-    def test_delete_notification_policy_tree(self, m):
-        JSON_RESPONSE = {"receiver": "grafana-default-email", "group_by": ["grafana_folder", "alertname"]}
+    def test_update_alertrule_disable_provenance(self):
+        """
+        cannot change provenance from 'api' to ''
+        """
+        with self.assertRaises(GrafanaServerError) as excinfo:
+            self.grafana.alertingprovisioning.update_alertrule(
+                alertrule_uid=self.alertrule_uid, alertrule=ALERTRULE, disable_provenance=True
+            )
+        self.assertEqual(excinfo.exception.status_code, 500)
 
-        m.delete("http://localhost/api/v1/provisioning/policies", json=JSON_RESPONSE)
-        self.grafana.alertingprovisioning.delete_notification_policy_tree()
+    def test_delete_notification_policy_tree(self):
+        response = self.grafana.alertingprovisioning.delete_notification_policy_tree()
+        self.assertEqual(response["group_by"], ["grafana_folder", "alertname"])
+        if Version(self.grafana.version) >= Version("12"):
+            self.assertEqual(response["receiver"], "empty")
+        else:
+            self.assertEqual(response["receiver"], "grafana-default-email")
 
-    @requests_mock.Mocker()
-    def test_delete_mute_timing(self, m):
-        JSON_RESPONSE = {}
-        m.delete("http://localhost/api/v1/provisioning/mute-timings/test-mute-timing", json=JSON_RESPONSE)
-        self.grafana.alertingprovisioning.delete_mute_timing("test-mute-timing")
-
-    @requests_mock.Mocker()
-    def test_get_mute_timing(self, m):
-        JSON_RESPONSE = {
-            "name": "test-mute-timing",
-            "time_intervals": [{}],
-            "version": "c0764d2988e93f94",
-            "provenance": "api",
-        }
-        m.get("http://localhost/api/v1/provisioning/mute-timings/test-mute-timing", json=JSON_RESPONSE)
+    def test_delete_mute_timing_success(self):
+        self.grafana.alertingprovisioning.create_mute_timing(
+            {
+                "name": "test-mute-timing",
+                "time_intervals": [{}],
+                "version": "c0764d2988e93f94",
+                "provenance": "api",
+            }
+        )
         response = self.grafana.alertingprovisioning.get_mute_timing("test-mute-timing")
         self.assertEqual(response["name"], "test-mute-timing")
+        self.grafana.alertingprovisioning.delete_mute_timing("test-mute-timing")
+
+    def test_delete_mute_timing_unknown(self):
+        with self.assertRaises(GrafanaClientError) as excinfo:
+            self.grafana.alertingprovisioning.get_mute_timing("unknown")
+        self.assertEqual(excinfo.exception.status_code, 404)
+        self.assertRegex(excinfo.exception.message, "Client Error 404: (null|NotFound)")
