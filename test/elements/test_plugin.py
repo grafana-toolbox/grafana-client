e6d15b6d9b5cb4c5a685ec382c9aa650efaa0057
Andreas Motl <andreas.motl@panodata.org>
2026-04-05 16:13:23 +0200

    Tests: Integration tests for plugins

--- test/elements/test_plugin.py
+++ test/elements/test_plugin.py
@@ -1,226 +1,112 @@
+import sys
 import unittest
 
+import pytest
+from verlib2 import Version
+
 from grafana_client import GrafanaApi
+from grafana_client.client import GrafanaClientError, GrafanaServerError
 
-from ..compat import requests_mock
+pytestmark = pytest.mark.integration
 
 
+@unittest.skipIf("unittest" in sys.argv[0], "Skipping unittest, please use pytest")
 class PluginTestCase(unittest.TestCase):
-    def setUp(self):
-        self.grafana = GrafanaApi(("admin", "admin"), host="localhost", url_path_prefix="", protocol="http")
+    @pytest.fixture(autouse=True)
+    def use_fixtures(self, grafana_provisioned: GrafanaApi):
+        self.grafana = grafana_provisioned
+        self.plugin_id = "marcusolsson-hourly-heatmap-panel"
 
-    @requests_mock.Mocker()
-    def test_list(self, m):
-        m.get(
-            "http://localhost/api/plugins?embedded=0",
-            json=[
-                {
-                    "name": "AWS Data Sources",
-                    "type": "app",
-                    "id": "aws-datasource-provisioner-app",
-                    "enabled": "true",
-                    "pinned": "true",
-                    "info": {
-                        "author": {"name": "Grafana Labs", "url": "https://grafana.com/"},
-                        "description": "AWS Datasource Provisioner",
-                        "links": [],
-                        "logos": {
-                            "small": "public/plugins/aws-datasource-provisioner-app/assets/logo-small.svg",
-                            "large": "public/plugins/aws-datasource-provisioner-app/assets/logo-large.svg",
-                        },
-                        "build": {
-                            "time": 1684358005873,
-                            "repo": "https://github.com/grafana/aws-datasource-provisioner-app",
-                            "branch": "main",
-                            "hash": "cae4b5ad22e487e9e0f81fca8ed801b36f87c733",
-                        },
-                        "screenshots": "",
-                        "version": "1.8.1",
-                        "updated": "2023-05-17",
-                    },
-                    "dependencies": {"grafanaDependency": ">=7.3.0", "grafanaVersion": ">=7.3.0", "plugins": []},
-                    "latestVersion": "",
-                    "hasUpdate": "false",
-                    "defaultNavUrl": "/plugins/aws-datasource-provisioner-app/page/aws-services",
-                    "category": "",
-                    "state": "",
-                    "signature": "valid",
-                    "signatureType": "grafana",
-                    "signatureOrg": "Grafana Labs",
-                },
-                {
-                    "name": "AWS IoT SiteWise",
-                    "type": "datasource",
-                    "id": "grafana-iot-sitewise-datasource",
-                    "enabled": "true",
-                    "pinned": "false",
-                    "info": {
-                        "author": {"name": "Grafana Labs", "url": "https://grafana.com"},
-                        "description": "A managed service to collect, store, organize and monitor data from industrial "
-                        "equipment",
-                        "links": [
-                            {"name": "Website", "url": "https://aws.amazon.com/iot-sitewise/"},
-                            {
-                                "name": "Issue Tracker",
-                                "url": "https://github.com/grafana/iot-sitewise-datasource/issues",
-                            },
-                        ],
-                        "logos": {
-                            "small": "public/plugins/grafana-iot-sitewise-datasource/img/sitewise.svg",
-                            "large": "public/plugins/grafana-iot-sitewise-datasource/img/sitewise.svg",
-                        },
-                        "build": {
-                            "time": 1692806526291,
-                            "repo": "https://github.com/grafana/iot-sitewise-datasource",
-                            "branch": "main",
-                            "hash": "a56fdf241cf677e025530d484472a0c838f05b79",
-                        },
-                        "screenshots": [],
-                        "version": "1.11.0",
-                        "updated": "2023-08-23",
-                    },
-                    "dependencies": {"grafanaDependency": ">=8.5.0", "grafanaVersion": "8.5.0", "plugins": []},
-                    "latestVersion": "",
-                    "hasUpdate": "false",
-                    "defaultNavUrl": "/plugins/grafana-iot-sitewise-datasource/",
-                    "category": "iot",
-                    "state": "",
-                    "signature": "unsigned",
-                    "signatureType": "",
-                    "signatureOrg": "",
-                },
-                {
-                    "name": "AWS IoT TwinMaker App",
-                    "type": "app",
-                    "id": "grafana-iot-twinmaker-app",
-                    "enabled": "true",
-                    "pinned": "true",
-                    "info": {
-                        "author": {"name": "AWS IoT TwinMaker", "url": "https://aws.amazon.com"},
-                        "description": "Create end-user 3D digital twin applications to monitor industrial operations "
-                        "with AWS IoT TwinMaker. AWS IoT TwinMaker is a service that makes it faster and"
-                        "easier for developers to create digital replicas of real-world systems, "
-                        "helping more customers realize the potential of digital twins to optimize operations.",
-                        "links": [{"name": "Website", "url": "https://github.com/grafana/grafana-iot-twinmaker-app"}],
-                        "logos": {
-                            "small": "public/plugins/grafana-iot-twinmaker-app/img/AWS-IoT-TwinMaker.png",
-                            "large": "public/plugins/grafana-iot-twinmaker-app/img/AWS-IoT-TwinMaker.png",
-                        },
-                        "build": {
-                            "time": 1684967559156,
-                            "repo": "https://github.com/grafana/grafana-iot-twinmaker-app",
-                            "branch": "main",
-                            "hash": "bc9c49f1b66c6146d17667e733b319eae967504b",
-                        },
-                        "screenshots": [],
-                        "version": "1.6.2",
-                        "updated": "2023-05-24",
-                    },
-                    "dependencies": {"grafanaDependency": ">=8.4.0", "grafanaVersion": "8.4.0", "plugins": []},
-                    "latestVersion": "",
-                    "hasUpdate": "false",
-                    "defaultNavUrl": "/plugins/grafana-iot-twinmaker-app/",
-                    "category": "iot",
-                    "state": "",
-                    "signature": "unsigned",
-                    "signatureType": "",
-                    "signatureOrg": "",
-                },
-                {
-                    "name": "Akumuli",
-                    "type": "datasource",
-                    "id": "akumuli-datasource",
-                    "enabled": "true",
-                    "pinned": "false",
-                    "info": {
-                        "author": {"name": "Eugene Lazin", "url": "https://akumuli.org"},
-                        "description": "Datasource plugin for Akumuli time-series database",
-                        "links": [{"name": "Project site", "url": "https://github.com/akumuli/Akumuli"}],
-                        "logos": {
-                            "small": "public/plugins/akumuli-datasource/img/logo.svg.png",
-                            "large": "public/plugins/akumuli-datasource/img/logo.svg.png",
-                        },
-                        "build": {},
-                        "screenshots": "",
-                        "version": "1.3.12",
-                        "updated": "2019-12-19",
-                    },
-                    "dependencies": {"grafanaDependency": "", "grafanaVersion": "4.5.x", "plugins": []},
-                    "latestVersion": "",
-                    "hasUpdate": "false",
-                    "defaultNavUrl": "/plugins/akumuli-datasource/",
-                    "category": "",
-                    "state": "",
-                    "signature": "unsigned",
-                    "signatureType": "",
-                    "signatureOrg": "",
-                },
-                {
-                    "name": "Alert list",
-                    "type": "panel",
-                    "id": "alertlist",
-                    "enabled": "true",
-                    "pinned": "false",
-                    "info": {
-                        "author": {"name": "Grafana Labs", "url": "https://grafana.com"},
-                        "description": "Shows list of alerts and their current status",
-                        "links": "null",
-                        "logos": {
-                            "small": "public/app/plugins/panel/alertlist/img/icn-singlestat-panel.svg",
-                            "large": "public/app/plugins/panel/alertlist/img/icn-singlestat-panel.svg",
-                        },
-                        "build": {},
-                        "screenshots": "null",
-                        "version": "",
-                        "updated": "",
-                    },
-                    "dependencies": {"grafanaDependency": "", "grafanaVersion": "*", "plugins": []},
-                    "latestVersion": "",
-                    "hasUpdate": "false",
-                    "defaultNavUrl": "/plugins/alertlist/",
-                    "category": "",
-                    "state": "",
-                    "signature": "internal",
-                    "signatureType": "",
-                    "signatureOrg": "",
-                },
-            ],
-        )
+        if Version(self.grafana.version) < Version("8"):
+            pytest.skip("Testing plugins only supported on Grafana 8 and higher.")
+
+    def test_list(self):
         plugins = self.grafana.plugin.list()
-        self.assertTrue(len(plugins), 6)
+        self.assertGreaterEqual(len(plugins), 6)
 
-    @requests_mock.Mocker()
-    def test_install(self, m):
-        m.post(
-            "http://localhost/api/plugins/alertlist/install",
-            json={"message": "Plugin alertlist installed"},
-        )
-        response = self.grafana.plugin.install(plugin_id="alertlist", version="1.3.12")
-        self.assertEqual(response["message"], "Plugin alertlist installed")
+    def test_install_success(self):
+        self.install_plugin(plugin_id=self.plugin_id)
 
-    @requests_mock.Mocker()
-    def test_uninstall(self, m):
-        m.post(
-            "http://localhost/api/plugins/alertlist/uninstall",
-            json={"message": "Plugin alertlist uninstalled"},
-        )
-        response = self.grafana.plugin.uninstall(plugin_id="alertlist")
-        self.assertEqual(response["message"], "Plugin alertlist uninstalled")
+    def test_install_collision_core(self):
+        """Validate installation collision with core plugin"""
+        with self.assertRaises(GrafanaClientError) as context:
+            self.grafana.plugin.install(plugin_id="text", version="0.8.15")
+        self.assertEqual(403, context.exception.status_code, "Wrong status code")
+        self.assertIn("Cannot install or change a Core plugin", context.exception.message)
 
-    @requests_mock.Mocker()
-    def test_health(self, m):
-        m.get(
-            "http://localhost/api/plugins/alertlist/health",
-            json={"message": "Plugin alertlist healthy"},
-        )
-        response = self.grafana.plugin.health(plugin_id="alertlist")
-        self.assertEqual(response["message"], "Plugin alertlist healthy")
+    def test_uninstall_success(self):
+        self.install_plugin(plugin_id=self.plugin_id)
+        self.grafana.plugin.uninstall(plugin_id=self.plugin_id)
 
-    @requests_mock.Mocker()
-    def test_metrics(self, m):
-        m.get(
-            "http://localhost/api/plugins/grafana-timestream-datasource/metrics",
-            json={"message": "Not found"},
-        )
-        response = self.grafana.plugin.metrics(plugin_id="grafana-timestream-datasource")
-        self.assertEqual(response["message"], "Not found")
+    def test_uninstall_core(self):
+        """Validate uninstallation of core plugin fails"""
+        with self.assertRaises(GrafanaClientError) as context:
+            self.grafana.plugin.uninstall(plugin_id="text")
+        self.assertEqual(403, context.exception.status_code, "Wrong status code")
+        self.assertIn("Cannot uninstall a Core plugin", context.exception.message)
+
+    def test_uninstall_unknown(self):
+        """Validate uninstallation of non-existent plugin fails"""
+        with self.assertRaises(GrafanaClientError) as context:
+            self.grafana.plugin.uninstall(plugin_id="unknown")
+        self.assertEqual(404, context.exception.status_code, "Wrong status code")
+        self.assertIn("Plugin not installed", context.exception.message)
+
+    def test_health_success(self):
+        """Validate health check after installing plugin"""
+        if Version(self.grafana.version) < Version("10"):
+            pytest.skip("grafana-testdata-datasource not available on Grafana 9 and before")
+        response = self.grafana.plugin.health(plugin_id="grafana-testdata-datasource")
+        self.assertEqual("Data source is working", response["message"])
+
+    def test_health_core(self):
+        """Standard core plugins can't do health checks by default"""
+        with self.assertRaises(GrafanaServerError) as context:
+            self.grafana.plugin.health(plugin_id="text")
+        if Version(self.grafana.version) >= Version("10"):
+            self.assertEqual(500, context.exception.status_code, "Wrong status code")
+            self.assertIn("Plugin unavailable", context.exception.message)
+        else:
+            self.assertEqual(503, context.exception.status_code, "Wrong status code")
+            self.assertIn("Plugin unavailable", context.exception.message)
+
+    def test_health_unknown(self):
+        """Validate failing health check on non-existent plugin"""
+        with self.assertRaises(GrafanaClientError) as context:
+            self.grafana.plugin.health(plugin_id="unknown")
+        self.assertEqual(404, context.exception.status_code, "Wrong status code")
+        self.assertRegex(context.exception.message, "(Plugin not found|Plugin not registered)")
+
+    def test_metrics_success(self):
+        plugin_id = "grafana-github-datasource"
+        self.install_plugin(plugin_id=plugin_id)
+        response = self.grafana.plugin.metrics(plugin_id=plugin_id)
+        self.assertIn("process_virtual_memory_max_bytes", response)
+
+    def test_metrics_not_implemented(self):
+        with self.assertRaises(GrafanaServerError) as context:
+            self.grafana.plugin.metrics(plugin_id="text")
+        if Version(self.grafana.version) >= Version("10"):
+            self.assertEqual(500, context.exception.status_code, "Wrong status code")
+            self.assertIn("An error occurred within the plugin", context.exception.message)
+        else:
+            self.assertEqual(503, context.exception.status_code, "Wrong status code")
+            self.assertIn("Plugin unavailable", context.exception.message)
+
+    def test_metrics_unknown(self):
+        with self.assertRaises(GrafanaClientError) as context:
+            self.grafana.plugin.metrics(plugin_id="unknown")
+        self.assertEqual(404, context.exception.status_code, "Wrong status code")
+        if Version(self.grafana.version) >= Version("10"):
+            self.assertIn("Plugin not registered", context.exception.message)
+        else:
+            self.assertIn("Plugin not found", context.exception.message)
+
+    def install_plugin(self, plugin_id: str):
+        try:
+            self.grafana.plugin.by_id(plugin_id=plugin_id)
+            return
+        except KeyError as ex:
+            if "Plugin not found" not in str(ex):
+                raise
+        self.grafana.plugin.install(plugin_id=plugin_id)
