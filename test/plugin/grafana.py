61e802d02405503ed44a071b66ea78f3eaca236f
Andreas Motl <andreas.motl@panodata.org>
2026-04-05 14:25:47 +0200

    Tests: Integration tests for organizations

--- test/plugin/grafana.py
+++ test/plugin/grafana.py
@@ -85,18 +85,64 @@ def team_basic(grafana_api):
 
 
 @pytest.fixture(scope="function")
+def user_testdrive(grafana_api, organization_testdrive):
+    try:
+        return grafana_api.admin.create_user(
+            {
+                "login": "testdrive",
+                "password": "secret",
+                "OrgId": organization_testdrive["orgId"],
+            }
+        )
+    except GrafanaClientError as err:
+        if err.status_code == 412:
+            return grafana_api.users.find_user("testdrive")
+        else:
+            raise
+
+
+@pytest.fixture()
+def organization_name() -> str:
+    return "Testdrive Org."
+
+
+@pytest.fixture(scope="function")
+def organization_testdrive(grafana_api, organization_name: str):
+    try:
+        return grafana_api.organization.create_organization(organization=organization_name)
+    except GrafanaClientError as err:
+        if err.status_code == 412:
+            return grafana_api.organization.find_organization(organization_name)
+        else:
+            raise
+
+
+@pytest.fixture(scope="function")
 def reset_grafana(grafana_api):
+
+    # Reset context.
+    grafana_api.organizations.switch_organization(organization_id=1)
+
+    # Reset folders and dashboards.
     for folder in grafana_api.folder.get_all_folders():
         try:
             grafana_api.folder.delete_folder(folder["uid"], force_delete_rules=True)
         except GrafanaClientError as ex:
             if ex.status_code != 404:
                 raise
+
+    # Reset data sources.
     for datasource in grafana_api.datasource.list_datasources():
         grafana_api.datasource.delete_datasource_by_uid(datasource["uid"])
+
+    # Reset users and organizations.
     for user in grafana_api.users.search_users():
         if user["id"] != 1:
             grafana_api.admin.delete_user(user["id"])
+    for org in grafana_api.organizations.list_organization():
+        if org["id"] != 1:
+            grafana_api.organizations.delete_organization(org["id"])
+    grafana_api.organizations.update_organization(organization_id=1, organization={"name": "Main Org."})
 
 
 # ruff: disable[ARG001]
